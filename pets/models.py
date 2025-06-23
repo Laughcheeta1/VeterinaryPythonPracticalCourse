16760680a6c706088ac790eccb911296c17eba9f
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-06-23 13:51:15 -0500

    Created the pet model

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from users.models import User
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class Pet(models.Model):
+    name = models.CharField(
+        max_length=20,
+        validators=[MinLengthValidator(1)]
+        )
+    birthday = models.DateField()
+    species = models.CharField(
+        max_length=20,
+        validators=[MinLengthValidator(1)]
+        )
+    race = models.CharField(
+        max_length=20,
+        validators=[MinLengthValidator(1)]
+        )
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL)

5427a9f84bef59e610cdd4d5f7760a8ac89f46c8
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-06-23 13:56:51 -0500

    Finished the appointment model

--- appointments/models.py
+++ appointments/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+from pets.models import Pet
 
 # Create your models here.
 class Appointment(models.Model):
     date = models.DateField()
     motive = models.CharField(max_length=250)
     diagnosis = models.CharField(max_length=1000)
-    # Todo add the pet
-
+    pet = models.ForeignKey(Pet, on_delete=models.SET_NULL)

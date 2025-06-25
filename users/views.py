971a1009d9dbdd6f93a59a455e8922faee5343bc
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-06-24 19:34:38 -0500

    Added the views (not functional) and the url paths

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landing_page(request):
+    return render(request, 'users/users_landing.html', {})
+
+def all_users(request):
+    return render(request, 'users/users_landing.html', {})
+
+def register_user(request):
+    return render(request, 'users/users_landing.html', {})
+
+def search_user(request):
+    return render(request, 'users/users_landing.html', {})
+
+def particular_user(request):
+    return render(request, 'users/users_landing.html', {})

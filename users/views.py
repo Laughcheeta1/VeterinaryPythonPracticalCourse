c8ef29205e2bc4f60109ac0f0bb1109c53a56e89
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-06-25 19:41:53 -0500

    Now the basic register works

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import User
+from .forms import UserForm
 
 # Create your views here.
 def landing_page(request):
@@ -13,7 +15,21 @@ def all_users(request):
     return render(request, 'users/all_users.html', context)
 
 def register_user(request):
-    return render(request, 'users/users_landing.html', {})
+    form = UserForm()
+
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print("It was succesfull")
+            # TODO: make the response be a page
+            return HttpResponse("Submission was successful")
+        else:
+            print("It was not succesfull")
+            return HttpResponse("unsecces")
+
+    context = { 'form': form }
+    return render(request, 'users/register_user.html', context)
 
 def search_user(request):
     return render(request, 'users/users_landing.html', {})

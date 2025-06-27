3c543715da5b22fc037d7cd21e55b14ea7770906
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-06-27 17:08:58 -0500

    We now have pets page

--- veterinary_clinic/urls.py
+++ veterinary_clinic/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_page.urls')),
     path('/users', include('users.urls')),
+    path('/pets', include('pets.urls')),
 ]

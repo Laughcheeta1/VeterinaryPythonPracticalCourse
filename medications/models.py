28063681eb6765ad781bb4b1318c5a232612f105
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-06-28 11:51:57 -0500

    Created the Medications_Sent table

--- medications/models.py
+++ medications/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from appointments.models import Annotation_Appointment
 
 # Create your models here.
 class Medications(models.Model):
@@ -8,3 +9,13 @@ class Medications(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Medications_Sent(models.Model):
+    annotation_appointment = models.ForeignKey(Annotation_Appointment, on_delete=models.CASCADE)
+    medication = models.ForeignKey(Medications, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    instructions = models.CharField()
+
+    def __str__(self):
+        return str(self.medication) + " " + str(self.quantity)

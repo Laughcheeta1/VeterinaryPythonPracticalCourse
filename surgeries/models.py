1b6e076bceb76e89042e063e306a3c8dbee3c911
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-06-27 20:26:52 -0500

    Made the Surgery and Sugery_Type tables

--- surgeries/models.py
+++ surgeries/models.py
@@ -1,8 +1,31 @@
 from django.db import models
 from pets.models import Pet
 
-# Create your models here.
+class Surgery_Type(models.Model):
+    name = models.CharField()
+
+    def __str__(self) -> str:
+        return self.name
+    
+    def to_dict(self):
+        return {
+            'name' : self.name
+        }
+
+
 class Surgery(models.Model):
     pet = models.ForeignKey(Pet, on_delete=models.SET_NULL, null=True)
     date = models.DateTimeField()
     surgeon = models.CharField()
+    type = models.ForeignKey(Surgery_Type, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.surgeon + " on " + str(self.pet)
+    
+    def to_dict(self):
+        return {
+            'pet': self.pet,
+            'date': self.date,
+            'surgeon': self.surgeon,
+            'type': self.type
+        }

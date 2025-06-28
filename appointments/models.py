c82af804d5dd36fffd63917e9f7ac173c6440bac
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-06-28 11:42:18 -0500

    Updated the appointments model

--- appointments/models.py
+++ appointments/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from pets.models import Pet
+from veterinarians.models import Veterinarian
 
 # Create your models here.
 class Appointment(models.Model):
-    date = models.DateTimeField()
-    motive = models.CharField(max_length=250)
-    diagnosis = models.CharField(max_length=1000)
     pet = models.ForeignKey(Pet, on_delete=models.SET_NULL, null=True)
+    motive = models.CharField(max_length=250)
+    date = models.DateTimeField()
+    diagnosis = models.CharField(max_length=1000)
+    veterinarian = models.ForeignKey(Veterinarian, on_delete=models.SET_NULL, null=True)
 
     def to_dict(self):
         return {

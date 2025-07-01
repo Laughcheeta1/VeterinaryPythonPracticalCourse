352c37e61ac4a9b77baee8a40324d32d3a7ba75e
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-07-01 10:55:10 -0500

    Updated the Pet form

--- pets/forms.py
+++ pets/forms.py
@@ -5,3 +5,25 @@ class PetForm(forms.ModelForm):
     class Meta:
         model = Pet
         fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'placeholder': 'Noa'
+            }),
+            'species': forms.TextInput(attrs={
+                'placeholder': 'Canino'
+            }),
+            'birthday': forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'form-control date-picker',
+            }),
+            'race': forms.TextInput(attrs={
+                'placeholder': 'Fresh Puddle'
+            }),
+        }
+        labels = {
+            'name': 'Nombre',
+            'birthday': 'Fecha de nacimiento',
+            'species': 'Especie',
+            'race': 'Raza',
+            'owner': 'Dueno',
+        }

0426e09e77a8c8a6bdfe2c917263dec4ad25f507
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-06-30 23:58:10 -0500

    Now the register page is beautiful

--- users/forms.py
+++ users/forms.py
@@ -5,3 +5,27 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'placeholder': 'Jhon Doe'
+            }),
+            'phone_number': forms.TextInput(attrs={
+                'placeholder': '+57 311 123 4567'
+            }),
+            'address': forms.TextInput(attrs={
+                'placeholder': 'Cra. 48 #18A 14 piso 5, Medellin'
+            }),
+            'birth_day': forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'form-control date-picker',
+            }),
+        }
+        help_texts = {
+            'phone_number': 'Puedes ingresar tu numero de celular con o sin el codigo del pais'
+        }
+        labels = {
+            'name': 'Nombre',
+            'phone_number': 'Numero de telefono',
+            'address': 'Direccion',
+            'birth_day': 'Fecha de nacimiento'
+        }

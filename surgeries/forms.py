e5238d06de2ac969e8d4beffb63c9d64a6f092cf
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-07-01 13:08:14 -0500

    Updated the surgery form

--- surgeries/forms.py
+++ surgeries/forms.py
@@ -1,7 +1,24 @@
 from django import forms
 from .models import Surgery
+from datetime import datetime
 
 class SurgeryForm(forms.ModelForm):
     class Meta:
         model = Surgery
         fields = '__all__'
+        widgets = {
+            # We are going to suppose the surgeries can be perfored at all hours
+            'date': forms.DateTimeInput(attrs={
+                'type': 'datetime-local',
+                'class': 'form-control date-picker',
+                'min': datetime.now().strftime('%Y-%m-%dT%H:%M'),
+                'max': f'{datetime.now().year}-12-31T11:59',
+                'step': '3600',
+                }),
+        }
+        labels = {
+            'pet': 'Mascota',
+            'type': 'Typo de cirugia',
+            'date': 'Fecha y hora',
+            'surgeon': 'Cirujano',
+        }
